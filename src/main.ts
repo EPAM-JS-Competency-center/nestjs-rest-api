4935b0f3ae576280d9cd835bc831db312cde81ce
Oleksandr Bondarenko <oleksandr_bondarenko2@epam.com>
2022-08-02 12:36:22 +0300

    feat: ec2 lab
    - upd script

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
 import { CustomExceptionFilter } from './exception-filters/custom.exception-filter';
 
 const appPort = process.env.PORT || 3000;
