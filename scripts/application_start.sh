6ddcb542e7b109a6bb6bce92f6c413f43b1cc8f6
dimakrl <dmytro_kyrylenko@epam.com>
2022-09-06 14:18:22 +0300

    Move pm2 install statement

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,5 @@
+pm2 stop all
+
 cd /home/ec2-user/express-app
 
 pm2 start npm -- run start:prod
--- scripts/install_project_dependencies.sh
+++ scripts/install_project_dependencies.sh
@@ -3,15 +3,13 @@
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/express-app
 
+npm install pm2 -g
+
 cd /home/ec2-user/express-app
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion (node is in path now)
-
-npm install -g pm2
-
-pm2 stop all
 
 npm install --legacy-peer-deps
 
