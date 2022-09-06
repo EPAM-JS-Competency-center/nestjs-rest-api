a8228c775e7836bf233925c1ad617f8d560338e3
dimakrl <dmytro_kyrylenko@epam.com>
2022-09-06 17:23:48 +0300

    Add logger

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import AppConfig from '../../app.config';
 import { adaptDynamooseOptions } from './adapters/dynamoose-options.adapter';
 import { DynamooseModule } from 'nestjs-dynamoose';
+import * as dynamoose from 'dynamoose';
 
 const databaseConfig = new AppConfig().build();
 
 @Module({
   imports: [DynamooseModule.forRoot(adaptDynamooseOptions(databaseConfig))],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  constructor() {
+    dynamoose.logger().then((logger) => {
+      logger.providers.set(console);
+    });
+  }
+}
