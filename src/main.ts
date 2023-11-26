45f16f6cc76726af47c708435022156e5ae1bdd3
tanyak1601 <tanyakk1601@gmail.com>
2023-11-26 19:39:27 +0300

    add env vars

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { CustomExceptionFilter } from './exception-filters/custom.exception-filter';
 
 async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomExceptionFilter());
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'http://localhost:3000',
   });
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
+  console.log(`App listen on port ${process.env.APP_PORT}`);
 }
 bootstrap();
